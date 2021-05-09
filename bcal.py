7144b55971acd42eb407569abe8cbaea1c54f60a
reddysunil1983 <reddy.hotmail.com>
2021-05-09 13:31:01 -0600

    Cal functions added

--- bcal.py
+++ bcal.py
@@ -1,1 +1,22 @@
-num1 = float(input("Enter num1: ")
+num1 = float(input("Enter num1: "))
+op = input("Enter Operator: ")
+num2 = float(input("Enter num2: "))
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "*":
+    pint(num1*num2)
+elif op == "/":
+    print(num1 - num2)
+elif op == "//":
+    print(num1 //2)
+elif op == "**":
+    print(num1 ** num2)
+else:
+    print("Invalid Operator")
+
+
+
+
+
